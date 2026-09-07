933d43ebe6ed6a5772138cc71aa13f0d4094c8bf
Tonim <99552287+ZpkDxGames@users.noreply.github.com>
2026-09-07 02:05:57 -0300

    feat(players): load responsive player workspace styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./control-room-2-1.css";
 import "./management-2-1.css";
 import "./visual-motion.css";
 import "./player-head.css";
+import "./player-workspace-2-3.css";
 import { UiPreferencesProvider } from "../components/ui-preferences-provider";
 
 export const metadata: Metadata = {
