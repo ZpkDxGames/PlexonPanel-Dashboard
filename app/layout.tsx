f1f4a84e7ba8214157ad2ac911204806e786cf19
Tonim <99552287+ZpkDxGames@users.noreply.github.com>
2026-09-07 10:44:10 -0300

    Load Dashboard 3.0 overview styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./player-head.css";
 import "./player-workspace-2-3.css";
 import "./control-room-3-0.css";
 import "./workspaces-3-0.css";
+import "./overview-3-0.css";
 import { UiPreferencesProvider } from "../components/ui-preferences-provider";
 
 export const metadata: Metadata = {
