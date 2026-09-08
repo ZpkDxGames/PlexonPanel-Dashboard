bb1736a5039f0804946ae63ea20852ca188c7b9f
Tonim <99552287+ZpkDxGames@users.noreply.github.com>
2026-09-08 01:41:16 -0300

    Load compact authority status overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./workspaces-3-0.css";
 import "./overview-3-0.css";
 import "./activity-history.css";
 import "./visual-responsive-refinement.css";
+import "./visual-responsive-authority.css";
 import { UiPreferencesProvider } from "../components/ui-preferences-provider";
 
 export const metadata: Metadata = {
